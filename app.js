14070b47c831b8ff467d59c07adbc0a0b04db21d
hasnaindayala09 <133885952+hasnaindayala09@users.noreply.github.com>
2023-05-19 17:20:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,267 @@
+// Chapter No 01
+
+// 1
+// alert("Your Name");
+// alert("Father Name");
+// alert("Email");
+// alert("Contact Number");
+// alert("Password");
+// alert("Where are you from? ");
+// 2
+// alert("How old you are?");
+
+// var user = prompt("Entry Your Name")
+
+// ================================================================
+
+// Chapter 2 (Variables for string)
+
+// 1
+// firstName = prompt("Enter Your Name");
+// fatherName = prompt("Enter Your Father Name");
+
+// console.log(firstName + " " + fatherName);
+// // ========================
+
+// 2
+// var teamName = "Pakistan";
+
+// alert("teamName");
+
+//3
+
+// var a = "Hi! Everyone";
+// console.log(a);
+
+// 4
+// var bestMan = "Quaid-e-Azam";
+// var RichMan = "Billgates";
+
+// alert(bestMan + " " + RichMan);
+// ===============================================================
+
+// Chapter 3 (Variables for numbers)
+
+//1
+// var caseQty = "";
+
+//2
+
+// var caseQty = "";
+// alert(caseQty);
+
+//3
+
+// var operator = "49.7";
+// var a = Math.ceil(operator)
+
+// console.log(a);
+
+//4
+
+// var c = 1 + 1;
+// console.log(c);
+
+//5
+// var product = 1000;
+// var GST = 17;
+// var TotalAmount = product + GST;
+
+// console.log(TotalAmount);
+
+//6
+// var item1 = 1050;
+// var item2 = 450;
+
+// var addition = item1 + item2;
+// console.log(addition);
+
+// Chapter 4 (Variable names Legal and Illegal)
+
+//1.
+// var productCost = 5.55;
+
+// console.log(productCost);
+
+// 2.
+// var Nameofband;
+// var nameOfBand;
+
+//3
+// var  userResponse = 205;
+
+//4
+// var bestMan = "Hasnain";
+// var goodMan = "Abdul Qadir";
+
+//5
+// legal
+// var userResponseTime ;
+// var abc ;
+// var xyz123 ;
+// var abc456 ;
+
+// illegal
+// var abc@2
+// var xyy#!%
+// var anp#$%
+
+// Chapter 5 (Math Expression I)
+
+//1
+// var a = 50;
+// var b = 10;
+// var c = a % b;
+
+// alert(c);
+
+//2
+// var per = 60 % 10;
+
+// console.log(per);
+
+//3
+// var largeNum = 0.9 * 5500;
+// console.log(largeNum);
+
+//4
+
+// var variable1 = 5;
+// var variable2 = 5;
+
+// var result = 5 - 5;
+
+// console.log(result);
+
+//5.
+// var item1 = 250
+// var item2 = 5
+// var remainder = item1 % item2;
+// console.log(remainder)
+//6.
+
+// alert (5*10)
+
+// Chapter : 06
+// `1
+// var a = 29;
+// a++;
+//  console.log(a);
+
+// 2
+//  var b = 60;
+//  --b;
+//  console.log(b);
+
+// 3
+//  var c = 80;
+//  var d = c++;
+//  console.log(d);
+
+// 4
+//  var e = 50;
+//  var f = --e;
+// console.log(f);
+//
+
+5;
+// var variable0 = 5;
+// var variable1 = variable0++;
+// console.log(variable1)
+
+// 6
+// var myNumber1 = 15;
+// myNumber1++;
+// alert("Updated Value: " + myNumber1);
+
+// //Chapter : 07//                                                // var calculatedNum = 2 + (2 * 6);
+
+// 1
+// var calculatedNum1 = (5 + 5) * 10;
+// console.log(calculatedNum1);
+
+2;
+// var calculatedNum6 = ((5 + 2) * 3) + 6;
+// console.log(calculatedNum6);
+
+3;
+// var Price = (4 + 4) * (2 + 10);
+// console.log(Price);
+
+// 4
+// var digit = 6 + (2 * (3 + 20));
+// console.log(digit);
+
+5;
+// var parament = (10 / 2) * 7;
+// console.log(parament);
+
+//                                      //Chapter : 08//
+1;
+// var num2 = "5" + "3";
+// console.log(num2);
+
+2;
+// var message = ("Hi," + "Hasnain");
+// alert(message);
+
+// 3
+// alert("43" + 5);
+// alert("Good " + "Morning");
+
+4;
+
+// var variable1 = "Pakistan ";
+// var variable2 = "Zindabad";
+// var result = variable1 + variable2;
+// alert(result);
+
+//                                                       //Chapter : 09//
+
+1;
+//  var firstName = prompt("Enter Your First Name");
+
+2;
+// var contry = prompt("Enter Your Contry Name","Pakistan");
+
+3;
+// var yourName = prompt("Enter Your Name");
+
+// 4
+// var DOB = prompt("Enter Your DOB","21-01-2000");
+
+// 5
+// var message = "Enter Your  Name";
+// var yourName = "Pakistan";
+// var p = prompt(message , yourName);
+// alert(p);
+
+// //Chapter : 10//
+
+1;
+// // var city = "Karachi";
+// // if(city == "Karachi"){
+// //     console.log("My City is Karachi");
+// }
+
+// 2
+// var xyz1 = 30;
+// var xyz2 = 30;
+// if(xyz1 === xyz2){
+//     var pr = prompt("Enter Your pasword");
+// }
+
+// 3
+// var zipCode = "1020";
+// if(zipCode === "1020"){
+//     alert("Islamabad");
+// }else{
+//     alert("Please Write Your Correct City");
+// }
+
+// 4
+// var ab = 600;
+// if(ab === 600){
+//         ab = 700;
+// }
+// console.log(ab);
